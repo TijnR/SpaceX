e2e4a503b757aa032abe924e21140af4c0c4fed1
TijnR <37410925+TijnR@users.noreply.github.com>
2020-03-05 20:59:52 +0100

    2.1v

--- javascript/bruh.js
+++ javascript/bruh.js
@@ -2,8 +2,17 @@ const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
+	var w = window.innerWidth;
+	console.log(w);
+
+	if (w < 768) {
+		nav.style.display = "none";
+	} else {
+	
+	}
 
 	burger.addEventListener('click', () => {
+		nav.style.display = "flex";
 		//toggle nav
 		nav.classList.toggle('nav-active');
 
@@ -11,10 +20,12 @@ const navSlide = () => {
 		navLinks.forEach((link, index) => {
 			if (link.style.animation) {
 				link.style.animation = "";
+				nav.style.display = "none";
 				console.log('na');
 
 			} else {
 				link.style.animation = `navLinkFade 0.5s ease forwards ${index/5+0.5}s`;
+
 
 			}
 
